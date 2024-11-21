40782d0533950e0ada703eaf0db82018d0e4e9f8
oliveiramarcelo12 <oliveiramarcelo198@gmail.com>
2024-11-21 20:28:05 -0300

    21/11

--- economia-solidaria-frontend/src/firebase.js
+++ economia-solidaria-frontend/src/firebase.js
@@ -1,12 +1,9 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth"; // Importa a autenticação
+import { getAnalytics } from "firebase/analytics"; // Importa o analytics
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyDBzrhVY41HFA88U78g8uD7MChbt4eJn2U",
   authDomain: "economiasolidaria-2db35.firebaseapp.com",
@@ -14,9 +11,15 @@ const firebaseConfig = {
   storageBucket: "economiasolidaria-2db35.firebasestorage.app",
   messagingSenderId: "1030267968737",
   appId: "1:1030267968737:web:ac871553b697644cc1aba0",
-  measurementId: "G-5BGG1TGQYW"
+  measurementId: "G-5BGG1TGQYW",
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const app = initializeApp(firebaseConfig); // Inicializa o Firebase
+const analytics = getAnalytics(app); // Inicializa o analytics (opcional)
+
+// Obtenha o serviço de autenticação
+const auth = getAuth(app); 
+
+// Exporte os serviços que você deseja usar
+export { app, auth, analytics };
