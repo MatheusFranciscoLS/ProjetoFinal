bacd43d375d0e999d7c1077d4cacff8e08420188
MateusOliveiraT25 <oliveiramateus1222@gmail.com>
2024-11-22 19:19:51 -0300

    22/11/24

--- economia-solidaria-frontend/src/components/Header.js
+++ economia-solidaria-frontend/src/components/Header.js
@@ -5,7 +5,7 @@ import "../styles/header.css";
 const Header = () => {
   return (
     <header className="header">
-      <div className="logo">FAZ O L</div>
+      <div className="logo">EconomiaSolidaria</div>
       <nav className="nav">
         <Link to="/register" className="nav-link">
           Cadastro
