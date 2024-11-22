b07953d49be8f933d5d6da5e67aa92b87ef9c86c
MateusOliveiraT25 <oliveiramateus1222@gmail.com>
2024-11-21 21:36:30 -0300

    21/11/24

--- economia-solidaria-frontend/src/App.js
+++ economia-solidaria-frontend/src/App.js
@@ -3,6 +3,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
+import Home from "./pages/Home";
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
       <Routes>
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/home" element={<Home />} />
       </Routes>
     </Router>
   );
