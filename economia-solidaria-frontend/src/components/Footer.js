090eb16ba216764982ad5af18454d9a0fec8c17e
MatheusFranciscoLS <matheusfran.ls@gmail.com>
2024-11-22 21:55:32 -0300

    22/11/24

--- economia-solidaria-frontend/src/components/Footer.js
+++ economia-solidaria-frontend/src/components/Footer.js
@@ -1,10 +1,10 @@
 import React from "react";
-import "../styles/header.css";
+import "../styles/footer.css";  // Corrigi o nome para "footer.css" caso o arquivo seja de estilo para o footer
 
 const Footer = () => {
   return (
     <footer className="footer">
-      <p>&copy; {new Date().getFullYear()} Minha Aplicação. Todos os direitos reservados 2024</p>
+      <p>&copy; {new Date().getFullYear()} Minha Aplicação. Todos os direitos reservados.</p>
     </footer>
   );
 };
